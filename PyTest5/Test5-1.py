9931e60936eb9ff739becb9eafe62851bb12a00b
weidaihong <15110225531@163.com>
2020-04-28 18:46:20 +0800

    爬虫代码提交

--- PyTest5/Test5-1.py
+++ PyTest5/Test5-1.py
@@ -1,6 +1,7 @@
 #定义一个函数，该函数可就收一个集合，用直接选择排序对该集合排序
-def  dirct_choose_sort(list):
-    k = 0
-    
+def  dirct_choose_sort(index,list):
+    num = list[index]
+    for i in range((index+1),len(list)):
+        list[i]
     return list
 print(dirct_choose_sort([100,5,3,8,6,4,5,6,5,9,7,64,45,3]))
---
+++ PyTest6/__init__.py
@@ -0,0 +1,3 @@
+class User():
+  def __init__(self,name='zangdan'):
+      self.name=name
